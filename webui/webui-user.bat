76e115efa32c356fd88db842d16d69725017a400
Marcin Stachura <stan7@tlen.pl>
2024-04-29 14:21:06 +0200

    webui starting parameters adjusted

--- webui/webui-user.bat
+++ webui/webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--no-download-sd-model --do-not-download-clip --skip-prepare-environment --skip-install --api --api-log --nowebui --api-server-stop --log-startup %*
+set COMMANDLINE_ARGS=--no-download-sd-model --do-not-download-clip --skip-prepare-environment --skip-install --api --api-log --nowebui --log-startup %*
 
 call webui.bat
